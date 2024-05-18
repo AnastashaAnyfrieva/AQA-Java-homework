17003f030189492ab737840c76c4437358b0f3cf
Anastasiya <anastashaanyfrieva@gmail.com>
2024-05-18 18:27:24 +0300

    Удаляем лишние строки в convertedArray, исправляем реализацию провеки размерности массива.

--- Lesson/src/main/java/by/LessonException/Runner.java
+++ Lesson/src/main/java/by/LessonException/Runner.java
@@ -1,15 +1,13 @@
 package by.LessonException;
 
 public class Runner {
-
     public static void main(String[] args) {
         String[][] array = {
                 {"11", "2", "3", "6"},
-                {"5", "25", "7", "8"},
+                {"5", "25", "7", "1"},
                 {"9", "10", "1", "2"},
                 {"13", "14", "1", "6"}
         };
-
         try {
             int sum = convertedArray(array);
             System.out.println("Сумма элементов: " + sum);
@@ -19,26 +17,25 @@ public class Runner {
     }
 
     public static int convertedArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+
         int rows = array.length;
-        int columns = array[0].length;
-
-        if (rows != 4 || columns != 4) {
-            throw new MyArraySizeException();
-        }
-
+        int columns = array.length;
+            if (array.length != 4)throw new MyArraySizeException();
+                for (String[] strings : array) {
+                    if (strings.length != 4) throw new MyArraySizeException();
+                }
         int sum = 0;
-
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 try {
-                    int x = Integer.parseInt(array[i][j]);
-                    sum += x;
+                    int value = Integer.parseInt(array[i][j]);
+                    sum += value;
                 } catch (NumberFormatException e) {
                     throw new MyArrayDataException(i, j);
                 }
             }
         }
-
         return sum;
     }
 }
+
