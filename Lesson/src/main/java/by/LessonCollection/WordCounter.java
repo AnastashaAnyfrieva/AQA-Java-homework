fd2da9e1c4d25e8485bf16f8854eacde8da09fa8
Anastasiya <anastashaanyfrieva@gmail.com>
2024-06-09 16:07:44 +0300

    Реализуем классы "Уникальные слова" и "Телефонный справочник" и реализуем их с помощью коллекций.reformat code

--- Lesson/src/main/java/by/LessonCollection/WordCounter.java
+++ Lesson/src/main/java/by/LessonCollection/WordCounter.java
@@ -6,26 +6,26 @@ import java.util.HashSet;
 
 public class WordCounter {
     public static void main(String[] args) {
-        String[] array = {"качели","мечта","Игра","пряник","радость",
-                "Сова","слова","горка","качели","банан",
-                "радость", "качели","яблоко",
-                "пряник","мечта","банан","улитка","Мечта","Поспать"};
+        String[] array = {"качели", "мечта", "Игра", "пряник", "радость",
+                "Сова", "слова", "горка", "качели", "банан",
+                "радость", "качели", "яблоко",
+                "пряник", "мечта", "банан", "улитка", "Мечта", "Поспать"};
         HashSet<String> uniqueWords = new HashSet<>();
         Collections.addAll(uniqueWords, array);
         System.out.println("Список уникальных слов: ");
-        for (String word : uniqueWords){
+        for (String word : uniqueWords) {
             System.out.println(word);
         }
         HashMap<String, Integer> wordCount = new HashMap<>();
-        for (String word : array){
-            if(wordCount.containsKey(word)){
-                wordCount.put(word, wordCount.get(word)+1);
-            }else {
-                wordCount.put(word,1);
+        for (String word : array) {
+            if (wordCount.containsKey(word)) {
+                wordCount.put(word, wordCount.get(word) + 1);
+            } else {
+                wordCount.put(word, 1);
             }
         }
         System.out.println("Количество повторений каждого слова: ");
-        for (String word : wordCount.keySet()){
+        for (String word : wordCount.keySet()) {
             System.out.println(word + ":" + wordCount.get(word));
         }
     }
--- Lesson/src/main/java/by/LessonCollection/phoneBook/PhoneBook.java
+++ Lesson/src/main/java/by/LessonCollection/phoneBook/PhoneBook.java
@@ -5,19 +5,22 @@ import java.util.HashMap;
 
 public class PhoneBook {
     private final HashMap<String, ArrayList<String>> phoneNumbers;
-    public PhoneBook(){
+
+    public PhoneBook() {
         phoneNumbers = new HashMap<>();
     }
-    public void add(String lastName, String phoneNumber){
-        if (phoneNumbers.containsKey(lastName)){
+
+    public void add(String lastName, String phoneNumber) {
+        if (phoneNumbers.containsKey(lastName)) {
             phoneNumbers.get(lastName).add(phoneNumber);
-        }else {
+        } else {
             ArrayList<String> numbers = new ArrayList<>();
             numbers.add(phoneNumber);
             phoneNumbers.put(lastName, numbers);
         }
     }
-    public ArrayList<String> get(String lastName){
+
+    public ArrayList<String> get(String lastName) {
         return phoneNumbers.get(lastName);
     }
 }
