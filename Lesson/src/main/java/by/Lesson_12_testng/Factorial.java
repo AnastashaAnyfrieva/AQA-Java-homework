9e45798dd8f4f69eefafbafd15fae3ac88939322
Anastasiya <anastashaanyfrieva@gmail.com>
2024-06-10 22:57:57 +0300

    Реализация класса Factorial, реализованы юнит-тесты  использованием фреймворка TestNG.

---
+++ Lesson/src/main/java/by/Lesson_12_testng/Factorial.java
@@ -0,0 +1,15 @@
+package by.Lesson_12_testng;
+
+public class Factorial {
+    public static int calculateFactorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateFactorial(4));
+    }
+}
---
+++ Lesson/src/test/java/by/Lesson_12_testng/FactorialTest.java
@@ -0,0 +1,39 @@
+package by.Lesson_12_testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static by.Lesson_12_testng.Factorial.calculateFactorial;
+
+public class FactorialTest {
+
+    @Test
+    public void testCalculateFactorialForZero() {
+        int result = calculateFactorial(0);
+        Assert.assertEquals(result, 1);
+    }
+
+    @Test
+    public void testCalculateFactorialForOne() {
+        int result = calculateFactorial(1);
+        Assert.assertEquals(result, 1);
+    }
+
+    @Test
+    public void testCalculateFactorialForThree() {
+        int result = calculateFactorial(3);
+        Assert.assertEquals(result, 6);
+    }
+
+    @Test
+    public void testCalculateFactorialForSeven() {
+        int result = calculateFactorial(7);
+        Assert.assertEquals(result, 5040);
+    }
+
+    @Test
+    public void testCalculateFactorialForNine() {
+        int result = calculateFactorial(9);
+        Assert.assertEquals(result, 362880);
+    }
+}
